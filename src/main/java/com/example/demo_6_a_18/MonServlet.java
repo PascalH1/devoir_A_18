92ecd560737bf5ce7723a56bbf00412b18b457e3
PascalH1 <123503124+PascalH1@users.noreply.github.com>
2024-01-10 16:48:51 -0500

    fin de la classe servlet

--- src/main/java/com/example/demo_6_a_18/MonServlet.java
+++ src/main/java/com/example/demo_6_a_18/MonServlet.java
@@ -5,3 +5,11 @@ import java.io.*;
 import jakarta.servlet.http.*;
 
 
+public class MonServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Bienvenue sur mon servlet</h1>");
+    }
+}
